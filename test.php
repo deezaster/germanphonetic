372d857ad2f5f89484c0eaba31d710dc1a3cfee8
Andy Theiler <andy.theiler@gmail.com>
2017-02-12 10:21:35 +0100

    Issue #7

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'x3m_soundex_ger.php';
+
+echo "<br>c = " . soundex_ger("c");
+echo "<br>röhre = " . soundex_ger("röhre");
+echo "<br>ärger = " . soundex_ger("ärger");
+echo "<br>aerger = " . soundex_ger("aerger");
+echo "<br>erker = " . soundex_ger("erker");
+echo "<br>berserker = " . soundex_ger("berserker");
+echo "<br>Yannik = " . soundex_ger("Yannik");
+echo "<br>Yannic = " . soundex_ger("Yannic");
+echo "<br>Dominic = " . soundex_ger("Dominic");
+echo "<br>Dominik = " . soundex_ger("Dominik");
+echo "<br>Andy = " . soundex_ger("Andy");
+echo "<br>andi = " . soundex_ger("andi");
+echo "<br>andie = " . soundex_ger("andie");
+echo "<br>andie = " . soundex_ger("andie");
+echo "<br>scx = " . soundex_ger("scx");
